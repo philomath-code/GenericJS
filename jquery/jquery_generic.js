b142bde4489d209ebb7a76984811662ed30b875b
philomath <philomathbooks@gmail.com>
2021-07-14 22:47:29 -0500

    started genericjs library

---
+++ jquery/jquery_generic.js
@@ -0,0 +1,6 @@
+//7.14.2021 - daniel medina
+//use: easily create variables with function in order to easily have the hashtag needed by jquery to manipulate elements by id
+function addHashTagForIdUse(variableName)
+{
+    return "#" + variableName;
+}
